61422cd3fb330b5cd4ae7c54061ca385eb8fdf6e
Vikram Seervi <84278571+thevikramseervi@users.noreply.github.com>
2026-06-30 06:10:29 +0000

    01. Contains Duplicate

---
+++ Arrays & Hashing/01_ContainsDuplicate.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+===========================================================
+Problem: Contains Duplicate
+
+Approaches:
+1. BruteForce
+TC = O(n^2)
+SC = O(1)
+
+2. Sorting
+TC = O(n log n)
+SC = O(1)
+
+3. HashSet or HashMap (actually hash set is better in this case as frequency of elements not required)
+TC = O(n)
+SC = O(n)
+===========================================================
+*/
+
+// ------------------- Brute Force ------------------------
+bool brute_force(vector<int> &nums)
+{
+    int n = nums.size();
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = i + 1; j < n; ++j)
+        {
+            if (nums[i] == nums[j])
+            {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+// --------------------- Sorting --------------------------
+bool sorting(vector<int> &nums)
+{
+    int n = nums.size();
+    sort(nums.begin(), nums.end());
+    for (int i = 0; i < n - 1; ++i)
+    {
+        if (nums[i] == nums[i + 1])
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+// -------------------- Hash Set --------------------------
+bool hash_set(vector<int> &nums)
+{
+    return unordered_set<int>(nums.begin(), nums.end()).size() != nums.size();
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    vector<int> nums(n);
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> nums[i];
+    }
+
+    cout << brute_force(nums) << "\n";
+    cout << sorting(nums) << "\n";
+    cout << hash_set(nums) << "\n";
+}
